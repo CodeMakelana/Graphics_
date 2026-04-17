bdb08cd759165fbf9aad615ea697a82688b199f7
CodeMakelana <makelanatshepiso2005@gmail.com>
2026-04-17 20:05:09 +0200

    sdf

---
+++ Graphics_P4/texture.h
@@ -0,0 +1,11 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <GL/glew.h>
+
+GLuint createColourTexture(int width, int height);
+GLuint createDisplacementTexture(int width, int height);
+GLuint createAlphaTexture(int width, int height);
+void bindTexture(GLuint textureID, int unit, GLuint shaderProgram, const char* uniformName);
+
+#endif
